156eeca600b63fd5cdd0204475e2bc35ad0e67f8
Bisma Khomeini <khomeini.bisma@gmail.com>
2021-04-29 13:58:21 +0700

    Final

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,27 @@
 console.log('Client side javascript file is loaded!')
+
+
+const weatherForm = document.querySelector('form')
+const searchData = document.querySelector('input')
+const messageOne = document.querySelector('#msg1')
+const messageTwo = document.querySelector('#msg2')
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const data = searchData.value
+    fetch(`http://localhost:3000/weather?address=${data}`).then((response) =>{
+        response.json().then(data =>{
+            if(data.error){
+                messageOne.textContent = data.error
+                messageTwo.textContent = '----------'
+            }else{
+                messageOne.textContent = `Location : ${data.location}`
+                messageTwo.textContent=`Forecast : ${data.forecastData}`
+            }
+        })
+    }).catch(e=>{
+        messageOne.textContent = 'Couldnt find location'
+        messageTwo.textContent = ' : "" ( '
+    })
+    console.log('Testing');
+})
+
--- src/app.js
+++ src/app.js
@@ -24,14 +24,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Bisma'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Bisma'
     })
 })
 
